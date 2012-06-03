fd82a5b6e04555119a733d214c9a87354353b800
Jonathan Chang <jchang641@gmail.com>
2012-06-03 15:14:06 -0700

    Add some error checking, fix a bug

--- simulate.R
+++ simulate.R
@@ -1,21 +1,27 @@
 #!/usr/bin/env Rscript
 
 ghetto.ou.sim <- function (params, sample.every=params[["generations"]]/100) {
-	# params: individuals, loci, mutation.rate, generations,
-	# bottleneck.proportion, bottleneck.times
+	required <- c("individuals", "generations", "mutation.rate", "loci",
+				  "bottleneck.times", "bottleneck.proportion")
 	
 	individuals <- params[["individuals"]]
 	generations <- params[["generations"]]
 	mutation.rate <- params[["mutation.rate"]]
+	bottleneck.times <- params[["bottleneck.times"]]
+	bottleneck.proportion <- params[["bottleneck.proportion"]]
 	loci <- params[["loci"]]
+	
+	if (generations %/% sample.every != generations / sample.every) {
+		stop("generations must be a multiple of sample.every")
+	}
 
-	bottleneck.at <- floor(params[["bottleneck.times"]] * generations)
+	bottleneck.at <- floor(bottleneck.times * generations)
 	# need to add 1 to account for first generation
 	sample.gens <- matrix(nrow=(generations / sample.every) + 1,
 						  ncol=individuals)
-	
+
 	prev.row <- rep.int(loci / 2, individuals)
-	
+
 	samprow <- 1
 	sample.gens[samprow, ] <- prev.row
 
@@ -25,21 +31,21 @@ ghetto.ou.sim <- function (params, sample.every=params[["generations"]]/100) {
 			sample.gens[samprow, ] <- prev.row
 			cat(i, mean(prev.row), var(prev.row), sep="\t", fill=T)
 		}
-		
+
 		cur <- prev.row
 		# drift
 		cur <- sample(cur, length(cur), replace=TRUE)
-		
+
 		# mutation
 		cur <- rbinom(cur, cur, mutation.rate) +
 			   rbinom(cur, loci - cur, 1 - mutation.rate)
-			   
+
 		# bottleneck
 		if (i %in% bottleneck.at) {
-			tmp <- sample(cur, floor(bottleneck.prop * length(cur)))
+			tmp <- sample(cur, floor(bottleneck.proportion * length(cur)))
 			cur <- sample(tmp, length(cur), replace=TRUE)
 		}
-		
+
 		prev.row <- cur
 	}
 	rownames(sample.gens) <- seq(0, generations, by=sample.every)
