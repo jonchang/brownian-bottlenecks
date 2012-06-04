44bd32dfdbfbb752fc0ece2114983b21beffa443
Tina Marcroft <tmarcroft@gmail.com>
2012-06-03 19:21:10 -0700

    Fixed output of graphing script
    Fixed output of graphing script as input is now a distribution of sigma
    values rather than raw character values. Most of the graphs only make
    sense when analyzing a set of raw character values so they were
    commented out.

--- Plots.R
+++ Plots.R
@@ -1,6 +1,6 @@
 #!/usr/bin/Rscript
 
-##################### Trait evolution plots ############################
+##################### Trait evolution plots ###################################
 
 ### For practice -- simulated browninan motion trait data
 
@@ -9,41 +9,41 @@
 time <- 1:1000000
 bottleneck <- c(0.2)
 
-######### READ IN FILES AND CALC SD ###################################
+######### READ IN FILES AND CALC SD ###########################################
 
 a <- matrix(data=NA, nrow=1000000, ncol=40)
 
 for (j in 1:20) {
-	filename <- paste("brownian_", i, ".txt", sep="")
+	filename <- paste("brownian_replicates/brownian_", j, ".txt", sep="")
 	datafile <- read.table(filename)
 	for (i in 1:1000000){
-		a[i,j] <- sd(datafile[i,])
+		a[i,j] <- apply(datafile[i,],1,sd)
 	}
 }
 
-for (j in 21:40) {
-	filename <- paste("brownian_", i, ".txt", sep="")
+for (j in 1:20) {
+	filename <- paste("brownian_replicates_2/brownian_", j, ".txt", sep="")
 	datafile <- read.table(filename)
 	for (i in 1:1000000){
-		a[i,j] <- sd(datafile[i,])
+		a[i,j+20] <- apply(datafile[i,],1,sd)
 	}
 }
 
 b <- matrix(data=NA, nrow=1000000, ncol=40)
 
 for (j in 1:20) {
-	filename <- paste("brownian_", i, ".txt", sep="")
+	filename <- paste("brownian_replicates_bottleneck_", j, ".txt", sep="")
 	datafile <- read.table(filename)
 	for (i in 1:1000000){
-		b[i,j] <- sd(datafile[i,])
+		b[i,j] <- apply(datafile[i,],1,sd)
 	}
 }
 
-for (j in 21:40) {
-	filename <- paste("brownian_", i, ".txt", sep="")
+for (j in 1:20) {
+	filename <- paste("brownian_replicates_2_bottleneck_", j, ".txt", sep="")
 	datafile <- read.table(filename)
 	for (i in 1:1000000){
-		b[i,j] <- sd(datafile[i,])
+		b[i,j+20] <- apply(datafile[i,],1,sd)
 	}
 }
 
@@ -67,7 +67,7 @@ library(ape)
 library(stringr)
 
 df <- as.data.frame(X.b)
-colnames(df) <- str_c("ind.",1:100)       
+#colnames(df) <- str_c("ind.",1:100)       
 
 ### calculate summary data
 
@@ -86,66 +86,71 @@ library(reshape2)
 #X2 = replicate(100, cumsum(c(0, rnorm(time_steps - 1))))
 
 d2 <- as.data.frame(X2)
-colnames(d2) <- str_c("ind.",1:100)
+#colnames(d2) <- str_c("ind.",1:100)
 
 trait.mean2 <- apply(d2, 1, mean)
 trait.sd2 <- apply(d2, 1, sd)
 
 ####################### RIBBON PLOTS OF TWO DATASETS ####################
 
-bind <- cbind(time, trait.mean1, trait.mean2) 
-comp <- as.data.frame(bind)
+#bind <- cbind(time, trait.mean1, trait.mean2) 
+#comp <- as.data.frame(bind)
 
-ggplot(data=comp, aes(time)) + geom_ribbon(aes(ymin=trait.mean1-2*trait.sd1, ymax=trait.mean1+2*trait.sd1), color="#FF0000", fill="#FF0000", alpha = 0.5) + geom_ribbon(aes(ymin=trait.mean2-2*trait.sd2, ymax=trait.mean2+2*trait.sd2), color="#0000FF", fill="#0000FF", alpha = 0.5) + geom_line(aes(y=trait.mean1), color="#FF0000") + geom_line(aes(y=trait.mean2), color="#0000FF") + scale_x_continuous("time (in generations)") + scale_y_continuous("trait value")  + geom_vline(xintercept=bottleneck*length(time), color="red")
+#ggplot(data=comp, aes(time)) + geom_ribbon(aes(ymin=trait.mean1-2*trait.sd1, ymax=trait.mean1+2*trait.sd1), color="#FF0000", fill="#FF0000", alpha = 0.5) + geom_ribbon(aes(ymin=trait.mean2-2*trait.sd2, ymax=trait.mean2+2*trait.sd2), color="#0000FF", fill="#0000FF", alpha = 0.5) + geom_line(aes(y=trait.mean1), color="#FF0000") + geom_line(aes(y=trait.mean2), color="#0000FF") + scale_x_continuous("time (in generations)") + scale_y_continuous("mean sd (trait value)")  + geom_vline(xintercept=bottleneck*length(time), color="red")
+#ggsave(filename=ribbonplotofmeanofsds.pdf)
 
 #################### CALCULATE DIFF FROM MEAN #########################
 
 ### full set of differences (for future multiple t-tests)
 
-diff2 <- d2-trait.mean2
-diff1 <- df-trait.mean1
+#diff2 <- d2-trait.mean2
+#diff1 <- df-trait.mean1
 
 ################## GRAPH MEAN CHANGE IN DIFF V. TIME ##################
 
-diff.mean1 <- apply(diff1, 1, mean)
-diff.mean2 <- apply(diff2, 1, mean)
-diff.mean.change <- diff.mean1-diff.mean2
-dmc.g <- cbind(time, diff.mean.change)
+#diff.mean1 <- apply(diff1, 1, mean)
+#diff.mean2 <- apply(diff2, 1, mean)
+#diff.mean.change <- diff.mean1-diff.mean2
+#dmc.g <- cbind(time, diff.mean.change)
 
-dmc.df <- as.data.frame(dmc.g)
+#dmc.df <- as.data.frame(dmc.g)
 
-ggplot(data=dmc.df, aes(x=time, y=diff.mean.change)) + geom_line() + geom_hline(yintercept=0, color="orange", linetype=2) + scale_x_continuous("time (in generations)") + scale_y_continuous("difference of mean difference") + geom_vline(xintercept=bottleneck*length(time), color="red")
+#ggplot(data=dmc.df, aes(x=time, y=diff.mean.change)) + geom_line() + geom_hline(yintercept=0, color="orange", linetype=2) + scale_x_continuous("time (in generations)") + scale_y_continuous("difference of mean difference") + geom_vline(xintercept=bottleneck*length(time), color="red")
+#ggsave(filename=meanchange.pdf)
 
 ################## GRAPH BOTH SDS V. TIME #############################
 
-sds <- cbind(time, trait.sd1, trait.sd2)
-sd.compare <- melt(as.data.frame(sds), id.vars="time")
+#sds <- cbind(time, trait.sd1, trait.sd2)
+#sd.compare <- melt(as.data.frame(sds), id.vars="time")
 
-ggplot(data=sd.compare, aes(x=time, y=value, color=variable)) + geom_line()  + geom_vline(xintercept=bottleneck*length(time), color="red")
+#ggplot(data=sd.compare, aes(x=time, y=value, color=variable)) + geom_line()  + geom_vline(xintercept=bottleneck*length(time), color="red")
 
 ################# CALCULATE P-VALUES OVER ALL GENERATIONS #############
 
 pval <- matrix(data = NA, nrow = length(time), ncol = 1)
 
 for (i in 1:length(time)){
-	temp <- t.test(diff1[i,], diff2[i,])
+	temp <- t.test(a[i,], b[i,])
 	pval[i] <- temp$p.value
 }
 
-pval[1] <- 1       # replace NA b/c couldn't perform T-test on exact same distributions
+#pval[1] <- 1       # replace NA b/c couldn't perform T-test on exact same distributions
 pval <- as.vector(pval)        # change matrix to vector
 
 ################# GRAPH CORRECTED CHANGE IN SD V. TIME with P-VALUE LEGEND ###############
 
-sd.diff <- trait.sd2 - trait.sd1
-trait.comb <- cbind(trait.sd1, trait.sd2)
+#sd.diff <- trait.sd2 - trait.sd1
+#trait.comb <- cbind(trait.sd1, trait.sd2)
+sd.diff <- trait.mean2 - trait.mean1
+trait.comb <- cbind(trait.mean1, trait.mean2)
 mean.traits <- apply(trait.comb, 1, mean)
 sd.corrected <- sd.diff/mean.traits
-sd.corrected[1] <- 0      # replace NA b/c can't divide by 0
+#sd.corrected[1] <- 0      # replace NA b/c can't divide by 0
 sd.diff.graph <- cbind(time, sd.corrected, pval)
 
 sd.diff.df <- as.data.frame(sd.diff.graph)
 
 ggplot(data=sd.diff.df, aes(x=time, y=sd.corrected, color="pval")) + geom_line() + geom_hline(yintercept=0, color="orange", linetype=2) + scale_x_continuous("time (in generations)") + scale_y_continuous("corrected change in sd") + geom_vline(xintercept=bottleneck*length(time), color="red")
+ggsave(filename="fuckingdone.pdf")
 
 
