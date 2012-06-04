5f6d11d3307c9ec0fe1cc741422ce96b6b00ee64
Jonathan Chang <jchang641@gmail.com>
2012-06-03 22:54:05 -0700

    add 10mil gen sim script

---
+++ run_10mil_gens.R
@@ -0,0 +1,70 @@
+#!/usr/bin/env Rscript
+
+library(multicore)
+library(reshape2)
+source("sim_library.R")
+
+num.runs <- 20
+folder <- "10mil_gens/"
+
+params <- list(individuals           = 1000,
+			   loci                  = 1000,
+			   mutation.rate         = 4 * 10^-6,
+			   generations           = 10^7,
+			   bottleneck.proportion = 0.01,
+			   bottleneck.times      = c(0.1, 0.11, 0.12, 0.13, 0.15)
+)
+bottleneck.times.single <- 0.1
+
+save.run <- function(parms, filename, options) {
+	results <- run.sim(parms, sample.every=parms$generations/10,
+					   options=options)
+	write.table(results, file=filename, col.names=FALSE)
+	lastrow <- results[nrow(results), ]
+	return(lastrow)
+}
+
+brownian.only <- function(parms, index) {
+	filename <- paste(folder, "brownian_", index,
+					  ".txt", sep="")
+	save.run(parms, filename, c("mutation"))
+}
+
+multi.bottleneck <- function(parms, index) {
+	filename <- paste(folder, "multi_bottleneck_", index,
+					  ".txt", sep="")
+	save.run(parms, filename, c("mutation", "bottleneck"))
+}
+
+single.bottleneck <- function(parms, index) {
+	filename <- paste(folder, "single_bottleneck_", index,
+					  ".txt", sep="")
+	save.run(parms, filename, c("mutation", "bottleneck"))
+}
+
+
+dir.create("10mil_gens", showWarnings=FALSE, recursive=TRUE)
+
+brown <- parallel(mclapply(seq_len(num.runs), brownian.only, parms=params))
+mbottle <- parallel(mclapply(seq_len(num.runs), multi.bottleneck, parms=params))
+	
+# different bottleneck time for the single run
+params[["bottleneck.times"]] <- bottleneck.times.single
+sbottle <- parallel(mclapply(seq_len(num.runs), single.bottleneck, parms=params))
+alljobs <- collect(list(brown, mbottle, sbottle))
+
+res <- alljobs[as.character(brown$pid)]
+df <- data.frame(res)
+colnames(df) <- seq_len(num.runs)
+write.table(df, file=paste(folder, "brownian_summary.txt", sep=""))
+
+res <- alljobs[as.character(mbottle$pid)]
+df <- data.frame(res)
+colnames(df) <- seq_len(num.runs)
+write.table(df, file=paste(folder, "multi_bottleneck_summary.txt", sep=""))
+
+res <- alljobs[as.character(sbottle$pid)]
+df <- data.frame(res)
+colnames(df) <- seq_len(num.runs)
+write.table(df, file=paste(folder, "single_bottleneck_summary.txt", sep=""))
+
