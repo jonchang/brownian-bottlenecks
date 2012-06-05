2ef72ab6932a4084e155516c78876ecb9e86b944
Jonathan Chang <jchang641@gmail.com>
2012-06-05 14:16:20 -0700

    Better plots and more customizable

--- ex_plots_brownian.R
+++ ex_plots_brownian.R
@@ -3,17 +3,28 @@
 library(ggplot2)
 library(reshape2)
 
-time.steps <- 100
-stuff <- replicate(50, cumsum(c(0, rnorm(time.steps - 1))))
+time.steps <- 200
+numplots <- 5
+width <- 5
+height <- 5
+
+stuff <- replicate(100, cumsum(c(0, rnorm(time.steps - 1))))
+
 df <- as.data.frame(stuff)
 df$time <- 1:time.steps
 melted <- melt(df, id.vars=c("time"))
-y.limits <- c(min(melted$value), max(melted$value))
 
-stepsize <- 20
+y.limits <- range(melted$value)
+stepsize <- time.steps / numplots
+
 for (i in seq(stepsize, time.steps, stepsize)) {
-	p <- ggplot(subset(melted, time < i), aes(time, value, group=variable)) + geom_line() + xlim(c(1, time.steps)) + ylim(y.limits)
-	ggsave(paste("ex_brownian_", i, ".pdf", sep=""), p)
-	q <- ggplot(subset(melted, time == i), aes(value)) + geom_histogram() + xlim(y.limits)
-	ggsave(paste("ex_brownian_hist_", i, ".pdf", sep=""), q)
+	counter <- i / time.steps * numplots
+	p <- ggplot(subset(melted, time < i), aes(time, value, group=variable)) + 
+		 geom_line(alpha=0.5) + xlim(c(1, time.steps)) + ylim(y.limits) + 
+		 geom_hline(aes(yintercept=mean(value)), color="red")
+	q <- ggplot(subset(melted, time == i), aes(value)) +
+		 geom_histogram(binwidth=diff(y.limits)/10) + xlim(y.limits) +
+		 geom_vline(aes(xintercept=mean(value)), color="red") + coord_flip()
+	ggsave(paste("ex_brownian_hist_", counter, ".pdf", sep=""), q, width=width, height=height)
+	ggsave(paste("ex_brownian_", counter, ".pdf", sep=""), p, width=width, height=height)
 }
