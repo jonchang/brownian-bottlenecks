0fa864152da789b3d57220075a0df689e4169486
Tina Marcroft <tmarcroft@gmail.com>
2012-06-05 00:26:43 -0700

    fixed kurtosis t.test bugs etc.

--- cleanplot.R
+++ cleanplot.R
@@ -128,6 +128,11 @@ multi.skewness <- as.data.frame(ll)
 #single.jarq_pval <- as.data.frame(nn)
 #multi.jarq_pval <- as.data.frame(oo)
 
+#fix initial kurtosis value#
+brownian.kurtosis[1,] <- -1
+single.kurtosis[1,] <- -1
+multi.kurtosis[1,] <- -1
+
 ##### CALCULATE MEANS OF SDS #####
 
 # mean for each time step #
@@ -177,7 +182,7 @@ sd.diff.graph <- cbind(time, sd.corrected, p.value)
 sd.diff.df <- as.data.frame(sd.diff.graph)
 
 ggplot(data=sd.diff.df, aes(x=time, y=sd.corrected)) + geom_line(aes(color=-p.value)) + geom_point(aes(color=-p.value, size=-p.value)) + geom_hline(yintercept=0, color="orange", linetype=2) + scale_x_continuous("time (in generations)") + scale_y_continuous("corrected change in var") + geom_vline(xintercept=c(bottleneck.single*realtime), color="red")
-ggsave(filename="single_v_noBN_final_VAR.pdf")
+ggsave(filename="single_v_noBN_final_VAR.pdf", height=5.5, width=8)
 
 ###############################################################
 ##### MULTIBOTTLENECK v. NO BOTTLENECK CORRECTED MEAN VAR #####
@@ -209,7 +214,7 @@ sd.diff.graph <- cbind(time, sd.corrected, p.value)
 sd.diff.df <- as.data.frame(sd.diff.graph)
 
 ggplot(data=sd.diff.df, aes(x=time, y=sd.corrected)) + geom_line(aes(color=-p.value)) + geom_point(aes(color=-p.value, size=-p.value)) + geom_hline(yintercept=0, color="orange", linetype=2) + scale_x_continuous("time (in generations)") + scale_y_continuous("corrected change in var") + geom_vline(xintercept=c(bottleneck.multi*realtime), color="red")
-ggsave(filename="multi_v_noBN_final_VAR.pdf")
+ggsave(filename="multi_v_noBN_final_VAR.pdf", height=5.5, width=8)
 
 #######################################################################
 ##### SINGLE BOTTLENECK v. NO BOTTLENECK DIFFERENCE MEAN KURTOSIS #####
@@ -220,12 +225,14 @@ ggsave(filename="multi_v_noBN_final_VAR.pdf")
 pval.s.k <- as.vector(matrix(data = NA, nrow = 201, ncol = 1))
 
 for (i in 1:201){
-	temp <- t.test(brownian.kurtosis[i,], single.kurtosis[i,])
-	pval.s.k[i] <- temp$p.value
+	if (i == 1){
+		pval.s.k[i] <-        1   # replace NaN b/c t-test fails
+	}
+	else {
+		temp <- t.test(brownian.kurtosis[i,], single.kurtosis[i,])
+		pval.s.k[i] <- temp$p.value
+	}
 }
-
-# replace NA b/c t-test between identical distributions
-pval.s.k[1] <- 1    
 
 p.value <- pval.s.k
 
@@ -237,7 +244,7 @@ k1.diff.graph <- cbind(time, k1.diff, p.value)
 k1.diff.df <- as.data.frame(k1.diff.graph)
 
 ggplot(data=k1.diff.df, aes(x=time, y=k1.diff)) + geom_line(aes(color=-p.value)) + geom_point(aes(color=-p.value, size=-p.value)) + geom_hline(yintercept=0, color="orange", linetype=2) + scale_x_continuous("time (in generations)") + scale_y_continuous("difference in kurtosis") + geom_vline(xintercept=c(bottleneck.single*realtime), color="red")
-ggsave(filename="single_v_noBN_final_KUR.pdf")
+ggsave(filename="single_v_noBN_final_KUR.pdf", height=5.5, width=8)
 
 ########################################################################
 ##### MULTIBOTTLENECK v. NO BOTTLENECK DIFFERENCE IN MEAN KURTOSIS #####
@@ -248,12 +255,14 @@ ggsave(filename="single_v_noBN_final_KUR.pdf")
 pval.m.k <- as.vector(matrix(data = NA, nrow = 201, ncol = 1))
 
 for (i in 1:201){
-	temp <- t.test(brownian.kurtosis[i,], multi.kurtosis[i,])
-	pval.m.k[i] <- temp$p.value
+	if (i == 1){
+		pval.m.k[i] <- 1               # replace NaN b/c t-test fails
+	}
+	else {
+		temp <- t.test(brownian.kurtosis[i,], multi.kurtosis[i,])
+		pval.m.k[i] <- temp$p.value
+	}
 }
-
-# replace NA b/c t-test between identical distributions
-pval.m.k[1] <- 1    
 
 p.value <- pval.m.k
 
@@ -265,7 +274,7 @@ k2.diff.graph <- cbind(time, k2.diff, p.value)
 k2.diff.df <- as.data.frame(k2.diff.graph)
 
 ggplot(data=k2.diff.df, aes(x=time, y=k2.diff)) + geom_line(aes(color=-p.value)) + geom_point(aes(color=-p.value, size=-p.value)) + geom_hline(yintercept=0, color="orange", linetype=2) + scale_x_continuous("time (in generations)") + scale_y_continuous("difference in kurtosis") + geom_vline(xintercept=c(bottleneck.multi*realtime), color="red")
-ggsave(filename="multi_v_noBN_final_KUR.pdf")
+ggsave(filename="multi_v_noBN_final_KUR.pdf", height=5.5, width=8)
 
 
 ####################################################################
@@ -273,7 +282,57 @@ ggsave(filename="multi_v_noBN_final_KUR.pdf")
 ####################################################################
 
 bind <- cbind(time, m.mean.b, m.mean.s) 
-comp <- melt(as.data.frame(bind), id.var="time")
+comp <- as.data.frame(bind)
 
-ggplot(data=comp, aes(time)) + geom_ribbon(aes(ymin=m.mean.b-2*trait.sd1, ymax=trait.mean1+2*trait.sd1), color="#FF0000", fill="#FF0000", alpha = 0.5) + geom_ribbon(aes(ymin=trait.mean2-2*trait.sd2, ymax=trait.mean2+2*trait.sd2), color="#0000FF", fill="#0000FF", alpha = 0.5) + geom_line(aes(y=trait.mean1), color="#FF0000") + geom_line(aes(y=trait.mean2), color="#0000FF") + scale_x_continuous("time (in generations)") + scale_y_continuous("mean sd (trait value)")  + geom_vline(xintercept=bottleneck*length(time), color="red")
-ggsave(filename=ribbonplotofmeanofsds.pdf)
+b.ymin <- m.mean.b-2*sqrt(v.mean.b)
+b.ymax <- m.mean.b+2*sqrt(v.mean.b)
+s.ymin <- m.mean.s-2*sqrt(v.mean.s)
+s.ymax <- m.mean.s+2*sqrt(v.mean.s)
+
+ggplot(data=comp, aes(time)) + geom_ribbon(aes(ymin=b.ymin, ymax=b.ymax), color="red", fill="red", alpha = 0.5) + geom_ribbon(aes(ymin=s.ymin, ymax=s.ymax), color="blue", fill="blue", alpha = 0.5) + geom_line(aes(y=m.mean.b), color="red") + geom_line(aes(y=m.mean.s), color="blue") + scale_x_continuous("time (in generations)") + scale_y_continuous("trait value")  + geom_vline(xintercept=bottleneck.single*realtime, color="black")
+ggsave(filename="ribbonplotofmeansNOvSING.pdf", height=5.5, width=8)
+
+# closeup
+
+closeup <- comp[1:40,]
+
+c.b.ymin <- b.ymin[1:40]
+c.b.ymax <- b.ymax[1:40]
+c.s.ymin <- s.ymin[1:40]
+c.s.ymax <- s.ymax[1:40]
+
+ggplot(data=closeup, aes(time)) + geom_ribbon(aes(ymin=c.b.ymin, ymax=c.b.ymax), color="red", fill="red", alpha = 0.5) + geom_ribbon(aes(ymin=c.s.ymin, ymax=c.s.ymax), color="blue", fill="blue", alpha = 0.5) + geom_line(aes(y=m.mean.b), color="red") + geom_line(aes(y=m.mean.s), color="blue") + scale_x_continuous("time (in generations)") + scale_y_continuous("trait value")  + geom_vline(xintercept=bottleneck.single*realtime, color="black")
+ggsave(filename="ribbonplotcloseupNOvSING.pdf", height=5.5, width=8)
+
+####################################################################
+##### RIBBON PLOT OF MULTI v. NO BOTTLNECK MEAN AND VARIANCES #####
+####################################################################
+
+bind2 <- cbind(time, m.mean.b, m.mean.m) 
+comp2 <- as.data.frame(bind)
+
+b.ymin <- m.mean.b-2*sqrt(v.mean.b)
+b.ymax <- m.mean.b+2*sqrt(v.mean.b)
+m.ymin <- m.mean.m-2*sqrt(v.mean.m)
+m.ymax <- m.mean.m+2*sqrt(v.mean.m)
+
+ggplot(data=comp2, aes(time)) + geom_ribbon(aes(ymin=b.ymin, ymax=b.ymax), color="red", fill="red", alpha = 0.5) + geom_ribbon(aes(ymin=m.ymin, ymax=m.ymax), color="blue", fill="blue", alpha = 0.5) + geom_line(aes(y=m.mean.b), color="red") + geom_line(aes(y=m.mean.m), color="blue") + scale_x_continuous("time (in generations)") + scale_y_continuous("trait value")  + geom_vline(xintercept=bottleneck.multi*realtime, color="black")
+ggsave(filename="ribbonplotofmeansNOvMULT.pdf", height=5.5, width=8)
+
+# closeup
+
+closeup2 <- comp[1:100,]
+
+c.b.ymin <- b.ymin[1:100]
+c.b.ymax <- b.ymax[1:100]
+c.m.ymin <- m.ymin[1:100]
+c.m.ymax <- m.ymax[1:100]
+
+ggplot(data=closeup2, aes(time)) + geom_ribbon(aes(ymin=c.b.ymin, ymax=c.b.ymax), color="red", fill="red", alpha = 0.5) + geom_ribbon(aes(ymin=c.m.ymin, ymax=c.m.ymax), color="blue", fill="blue", alpha = 0.5) + geom_line(aes(y=m.mean.b), color="red") + geom_line(aes(y=m.mean.s), color="blue") + scale_x_continuous("time (in generations)") + scale_y_continuous("trait value")  + geom_vline(xintercept=bottleneck.multi*realtime, color="black")
+ggsave(filename="ribbonplotcloseupNOvMULT.pdf", height=5.5, width=8)
+
+
+##### SKEW OVER TIME #####
+
+##### KURTOSIS OVER TIME #####
+
