68238c078086284abd9c4dbc23e52096f3fae204
Jonathan Chang <jchang641@gmail.com>
2012-06-08 12:52:46 -0700

    bigger font for plots

--- create_brownian_example_plots.R
+++ create_brownian_example_plots.R
@@ -5,8 +5,10 @@ library(reshape2)
 
 time.steps <- 200
 numplots <- 5
-width <- 5
-height <- 5
+width <- 7
+height <- 7
+tt <- theme_text(size=20)
+opt <- opts(axis.text.y=tt, axis.text.x=tt, axis.title.x=tt, axis.title.y=theme_text(size=20, angle=90))
 
 stuff <- replicate(100, cumsum(c(0, rnorm(time.steps - 1))))
 
@@ -21,10 +23,11 @@ for (i in seq(stepsize, time.steps, stepsize)) {
 	counter <- i / time.steps * numplots
 	p <- ggplot(subset(melted, time < i), aes(time, value, group=variable)) + 
 		 geom_line(alpha=0.5) + xlim(c(1, time.steps)) + ylim(y.limits) + 
-		 geom_hline(aes(yintercept=mean(value)), color="red")
-	q <- ggplot(subset(melted, time == i), aes(value)) +
+		 geom_hline(aes(yintercept=mean(value)), color="red") + opt
+	q <- ggplot(subset(melted, time == i), aes(value)) + opt +
 		 geom_histogram(binwidth=diff(y.limits)/10) + xlim(y.limits) +
-		 geom_vline(aes(xintercept=mean(value)), color="red") + coord_flip()
-	ggsave(paste("ex_brownian_hist_", counter, ".pdf", sep=""), q, width=width, height=height)
-	ggsave(paste("ex_brownian_", counter, ".pdf", sep=""), p, width=width, height=height)
+		 geom_vline(aes(xintercept=mean(value)), color="red") + coord_flip() +
+		 ylim(c(0, 50))
+	ggsave(paste("ex_brownian_hist_", counter, ".pdf", sep=""), q, width=width, height=height) 
+	ggsave(paste("ex_brownian_", counter, ".pdf", sep=""), p, width=width, height=height) 
 }
